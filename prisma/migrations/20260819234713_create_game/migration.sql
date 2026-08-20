87137d59f574673c696d52218fddf229355c99d7
edu <eduardogfrainer@gmail.com>
2026-08-19 21:11:12 -0300

    migrations

---
+++ prisma/migrations/20260819234713_create_game/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE `Game` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `appId` INTEGER NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `image` VARCHAR(191) NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `Game_appId_key`(`appId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
